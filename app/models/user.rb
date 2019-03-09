e5d49d4f6070a016f1eaa0bcd696d809bfba4960
Jared Harbison <jared.harbison@gmail.com>
2019-03-09 17:05:33 +0000

    Automatically backed up by Learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   validates_presence_of :name, :email, :password
-  def change 
+  def change
     create_table :users do |t|
       t.string :name
-      t.string :email 
+      t.string :email
       t.string :password
-    end 
+    end
   end 
 end
